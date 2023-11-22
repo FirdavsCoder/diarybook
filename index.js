a2c1d3e41bb57a4e3ede3e2b0206bc457c348749
FirdavsCoder <firdavsyorkulov@gmail.com>
2023-11-22 05:40:39 +0500

    updated

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express")
+const dotenv = require("dotenv")
+
+
+// Initial env variables
+dotenv.config()
+const app = express()
+
+
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
+    console.log(`Server running on port: http://localhost:${PORT}`);
+})
