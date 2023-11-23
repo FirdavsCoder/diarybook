98669386ea928ed0a369c00c1c7aadc60c7961e6
FirdavsCoder <firdavsyorkulov@gmail.com>
2023-11-24 03:34:29 +0500

    updated

--- models/index.js
+++ models/index.js
@@ -14,12 +14,14 @@ db.diary = require("./diary.model")(sequelize, Sequelize)
 db.comment = require("./comment.model")(sequelize, Sequelize)
 
 
-db.diary.hasMany(db.comment, {as: 'comment'})
-db.comment.belongsTo(db.diary, {
-    foreignKey: "diaryId",
-    as: "diary",
+db.diary.hasMany(db.comment, {
+    as: 'comment',
     onDelete: "CASCADE",
     constraints: true
+})
+db.comment.belongsTo(db.diary, {
+    foreignKey: "diaryId",
+    as: "diary"
 })
 
 
