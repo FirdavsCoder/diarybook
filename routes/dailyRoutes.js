20c33cfd5ba20dce1686d768378796749337cf80
FirdavsCoder <firdavsyorkulov@gmail.com>
2023-11-24 00:41:14 +0500

    Update dailyRoutes.js

--- routes/dailyRoutes.js
+++ routes/dailyRoutes.js
@@ -10,7 +10,7 @@ router.get("/my", getMyDiary)
 router.get("/:id", getDiaryById)
 router.get("/update/:id", updateDiaryGetPage)
 router.post("/add", addNewDiary)
-router.post("/diary/update", updateDiary)
+router.post("/update", updateDiary)
 
 
 
