60601cf53f8dc8b8316aea1c4af2a47fcdc8a6cc
FirdavsCoder <firdavsyorkulov@gmail.com>
2023-11-25 04:59:13 +0500

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,12 +3,14 @@ const router = express.Router()
 const {
     getLoginPage, loginUser, logout, getRegisterPage, registerUser
 } = require("../controllers/auth.controller")
+const { guest } = require("../middlewares/auth")
 
-router.get("/login", getLoginPage)
-router.post("/login", loginUser)
+
+router.get("/login", guest, getLoginPage)
+router.post("/login", guest, loginUser)
 router.get("/logout", logout)
-router.get("/register", getRegisterPage)
-router.post("/registration", registerUser)
+router.get("/register", guest, getRegisterPage)
+router.post("/registration", guest, registerUser)
 
 
 module.exports = router
