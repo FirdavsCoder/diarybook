2027a64c11dc95b544af5504a9a32bc3976f335a
FirdavsCoder <firdavsyorkulov@gmail.com>
2023-11-25 04:04:05 +0500

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const router = express.Router()
+const bcrypt = require("bcryptjs")
 const {
     getLoginPage, loginUser, logout, getRegisterPage, registerUser
 } = require("../controllers/auth.controller")
